3fb63b26bbce3b4d0b1298fe7e33dea0c1b7c854
მაცი <93665709+maci03@users.noreply.github.com>
2023-10-03 23:38:20 +0400

    worked on functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-     
+     <Logo />;
+     <Form />;
+     <PackingList />;
+     <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo(){
+  return <h1>Far Away</h1>
+}
+
+function Form(){
+  return(
+    <div className='add-form'>
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+
+function PackingList(){
+  return <div className='list'>LIST</div>;
+}
+
+
+function Stats(){
+  return <footer className='stats'>
+    you have X items on your list, and you already packed x (X%)
+  </footer>
+}
+
+
