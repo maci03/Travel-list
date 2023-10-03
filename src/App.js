b18555c035482ef82a16288d23490c69ca531447
მაცი <93665709+maci03@users.noreply.github.com>
2023-10-03 23:55:43 +0400

    added initialitems

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+const initialItems = [
+  {id: 1, description: "Passports", quantity: 2, packed: false},
+  {id: 2, description: "Socks", quantity: 12, packed: false},
+]
+
+
+
 
 export default function App() {
   return (
@@ -23,8 +30,20 @@ function Form(){
 }
 
 function PackingList(){
-  return <div className='list'>LIST</div>;
+  return <ul className='list'>
+    {initialItems.map((items) => (
+      <item item={item} />
+    ))}
+  </ul>;
+
 }
+
+function item({item}){
+  return <li>{item.description}</li>
+}
+
+
+
 
 
 function Stats(){
